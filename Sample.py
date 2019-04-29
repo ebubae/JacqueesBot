62230cc1124086eaea6a759fa6c65a076902be20
Devin Howard <devinhoward@devins-mbp.devices.brown.edu>
2019-04-29 14:03:04 -0400

    added sample class

---
+++ Sample.py
@@ -0,0 +1,10 @@
+class Sample:
+	def __init__(self, name, instrument, length, is_pitched, pitch, embedding):
+		self.name = name
+		self.is_pitched = is_pitched
+		self.pitch = pitch
+		self.instrument = instrument
+		self.length = length
+		self.embedding = embedding
+		
+
