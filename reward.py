59ea109a3bffdd569b1809c2bc9c3f497e5a0a25
Ebube Chuba <ebubae@users.noreply.github.com>
2019-05-07 15:34:01 -0400

    JacqueesBot v1.0

--- reward.py
+++ reward.py
@@ -10,7 +10,7 @@ from preprocess import process_loaded_audio
 from keras.models import load_model
 from keras.models import Model
 
-from sys import argv
+from sys import argv, maxsize
 
 music_model = load_model('music_model.h5')
 embed_model = Model(inputs=music_model.input, outputs=music_model.get_layer('embed').output)
@@ -22,28 +22,41 @@ def partition_audio(aud, sr, eps, block_size=3):
 
   Return a generator of all ready to be processed audio blocks
   '''
-  return (aud[off: block_size * sr + off] for off in range(0, len(aud), sr))
+  off = 0
+  while len(aud[off:]) > block_size * sr:
+    yield aud[off:off + block_size * sr]
+    off += sr
+  #return (aud[off: block_size * sr + off] for off in range(0, sr * round(len(aud)/sr), sr))
   #return (load(aud_path, offset=i, duration=block_size)[0] for i in xrange(int(duration) - block_size + 1))
 
+
+def length_coeff(i_size, o_size, delta):
+  return abs(-0.5 / i_size * o_size + 1) if o_size < i_size + delta else maxsize
 
 def get_reward(i_path, o_path, eps, delta, alpha=0.5, beta=12.0):
   input_audio, sr = load(i_path)
   output_audio, _ = load(o_path)
-  input_blocks  = partition_audio(i_path, sr, eps)
-  output_blocks = partition_audio(o_path, sr, eps)
+  i_size = len(input_audio)
+  o_size = len(output_audio)
+  coeff = length_coeff(i_size, o_size, delta)
+  
+  input_blocks  = partition_audio(input_audio, sr, eps)
+  output_blocks = partition_audio(output_audio, sr, eps)
   processed_input  = [process_loaded_audio(a) for a in input_blocks]
   processed_output = [process_loaded_audio(a) for a in output_blocks]
   clip_len = min(len(processed_input), len(processed_output))
-  processed_input = np.array(input_audio[:clip_len])
-  processed_output = np.array(output_audio[:clip_len])
+  processed_input = np.array(processed_input[:clip_len])
+  processed_output = np.array(processed_output[:clip_len])
   i_embed = embed_model.predict(processed_input)
   o_embed = embed_model.predict(processed_output)
-  i_chromo = chroma_cqt(input_audio, sr)
-  o_chromo = chroma_cqt(output_audio, sr)
-  #print(i_embed.shape)
-  #print(o_embed.shape)
-  # TODO: length correction to input and 
-  return alpha * np.linalg.norm(i_embed - o_embed) + beta * np.linalg.norm(i_chromo - o_chromo)
+  clip_len = min(len(input_audio), len(output_audio))
+  i_chromo = chroma_cqt(input_audio[:clip_len], sr)
+  o_chromo = chroma_cqt(output_audio[:clip_len], sr)
+  timbre_reward = 0#alpha * np.linalg.norm(i_embed - o_embed)
+  pitch_reward = beta * np.linalg.norm(i_chromo - o_chromo)
+  print("timbre cost: {}".format(timbre_reward))
+  print("pitch cost: {}".format(pitch_reward))
+  return -(timbre_reward + pitch_reward) * coeff
 
 if __name__ == '__main__':
   i_path = argv[1]
