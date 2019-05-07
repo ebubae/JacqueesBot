7ea639ca5b5d781b2f75722db78498ecff136e13
Ebube Chuba <ebubae@users.noreply.github.com>
2019-05-07 15:25:35 -0400

    JacqueesBot v1.0

--- agent.py
+++ agent.py
@@ -6,16 +6,17 @@ from state import State
 from mcts import mcts
 
 # time limit for MCTS in seconds
-MAX_RUNTIME = 120
+MAX_RUNTIME = 600
 
 # promt user for set up data, save json, cd into project dir
 exp_dir = setup()
 chdir(exp_dir)
 
 init_state = State('config.json')
+#RPR_ShowConsoleMsg("GOth here")
 mcts = mcts(timeLimit=MAX_RUNTIME * 1000)
-mcts.search(initalState=init_state)
-final_state = mcts.getBestChild(mcts.root, 0)
+mcts.search(initialState=init_state)
+final_state = mcts.getBestChild(mcts.root, 0).state
 final_state.export()
 # TODO: pickle mcts tree`
 print("Final reward: {}".format(final_state.getReward()))
